c411a7a2aa3297c3e1f503a187b6c992e112a98a
nandhithakamal <nandithk@thoughtworks.com>
2018-10-23 14:37:47 +0530

    Running docker in script

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def root():
+    return "Welcome to the registry!"
+
+@app.route("/hello")
 def hello():
     return "Hello world!"
 
